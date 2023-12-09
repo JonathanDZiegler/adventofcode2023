0933f809ad59bd6c4805fd4d172f165373b89d27
Jonathan <jonathan@cradle.bio>
2023-12-09 11:18:01 +0100

    updated template and helpers

--- helpers.py
+++ helpers.py
@@ -16,6 +16,6 @@ def load_str(path:str):
 
 def load_int(path:str):
     lines = input_lines(read_input_from_file(path))
-    chars = [l.split(',') for l in lines]
+    chars = [l.split() for l in lines]
     return [[int(c) for c in l] for l in chars]
     # return lines
--- template.py
+++ template.py
@@ -1,10 +1,19 @@
 from helpers import load
 import os
+import time
 
-def solve():
+def solve(part:int):
     data_path = f'data_d_{os.path.basename(__file__)[:-2]}csv'
     data = load(data_path)
     print("\n".join(data))
 
 if __name__=="__main__":
-    solve()
+    start = time.perf_counter()
+    # solve(1)
+    intermediate = time.perf_counter()
+    print(
+        f"Computation time for part 1: {1000*(intermediate-start):0.3f} milliseconds."
+    )
+    # solve(2)
+    stop = time.perf_counter()
+    print(f"Computation time for part 2: {1000*(stop-intermediate):0.3f} milliseconds.")
